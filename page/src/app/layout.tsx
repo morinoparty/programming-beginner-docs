fcd54fad5f6575a59c790c7a5860ccb635e01385
Nlkomaru <nikomaru@nikomaru.dev>
2024-06-28 17:20:30 +0900

    Add slide list

--- page/src/app/layout.tsx
+++ page/src/app/layout.tsx
@@ -1,8 +1,16 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Noto_Sans_JP } from 'next/font/google'
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const notoSansJP = Noto_Sans_JP({
+  subsets: ['latin'],
+  variable: '--font-noto-sans-jp',
+  // weight: 'variable', // default なので不要。バリアブルフォントでなければ必要
+  // display: 'swap', // default なので不要
+  // preload: true, // default なので不要
+  // adjustFontFallback: true, // next/font/google で default なので不要
+  // fallback: ['system-ui', 'arial'], // local font fallback なので不要
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={notoSansJP.className}>{children}</body>
     </html>
   );
 }
--- page/src/app/page.tsx
+++ page/src/app/page.tsx
@@ -1,5 +1,10 @@
 import {css} from '@/styled-system/css';
+import * as Card from '~/components/ui/card'
+import React from "react";
 import Link from "next/link";
+import {IconButton} from "~/components/ui/icon-button";
+import {FileText, Presentation} from "lucide-react";
+import {Presence} from "@ark-ui/react";
 
 export const runtime = "edge";
 
@@ -9,27 +14,124 @@ export default async function Home() {
     if (!bucket) {
         throw new Error("BUCKET is not defined in the environment");
     }
-    let list =(await bucket.list()).objects.map((o) => o.key.split("/")[0])
+    let list = (await bucket.list()).objects.map((o) => o.key.split("/")[0])
     let data = Array.from(new Set(list))
 
+    let slideList: SlideGroup[] = [
+        {
+            "Group": "環境構築",
+            "list": [
+                {slide: "java-1", title: "環境構築-1"},
+            ]
+        }
+    ]
+
+    slideList.forEach((group) => {
+        group.list.map((slide) => {
+            data = data.filter(n => n !== slide.slide);
+        })
+    })
+
+    if (data.length !== 0) {
+        slideList.push({
+            "Group": "その他",
+            "list": data.map((slide) => {
+                return {slide: slide, title: slide}
+            })
+        })
+    }
 
     return (
         <>
-            <div className={css({fontSize: "2xl", fontWeight: 'bold'})}>Hello 🐼!</div>
+            <div className={css({fontSize: "xl", fontWeight: 'bold', paddingBottom: "30px"})}>Slide List</div>
 
-            <div className={css({fontSize: "xl", fontWeight: 'bold'})}>Slide List</div>
-            <ul>
-                {data.map((slide) => (
-                    <li key={slide}>
-                        <Link href={`https://slide.moripa.nikomaru.dev/${slide}/index.html`} rel="noopener noreferrer"
-                              target="_blank">
-                            {slide}
-                        </Link>
-                    </li>
-                ))}
-            </ul>
+            {slideList.map((slide) => (
+                <>
+                    <h2 className={css({fontSize: "xl", fontWeight: 'bold', paddingBottom: "10px"})}>{slide.Group}</h2>
+                    <ul>
+                        {slide.list
+                            .map((slideName) => getSlideInfo(slideName.slide, slideName.title))
+                            .map((slideInfo) => (
+                                <li key={slideInfo.slideTitle}>
+                                    <Card.Root width="xs" className={css({
+                                        outline: "1px solid",
+                                        outlineColor: "gray.light.a5",
+                                        margin: "20px"
+                                    })}>
+                                        <Card.Header className={css({
+                                            padding: "0px 0px 0px 0px",
+                                            outline: "1px solid",
+                                            outlineColor: "black"
+                                        })}>
+                                            <img src={slideInfo.imageUrl}
+                                                 alt={slideInfo.slideTitle}/>
+                                        </Card.Header>
+                                        <hr style={{
+                                            color: "gray",
+                                            backgroundColor: "gray",
+                                            height: 2
+                                        }}/>
+                                        <Card.Body className={css({padding: "5px 0px 0px 20px"})}>
+                                            <div className={css({fontSize: "xl"})}>{slideInfo.slideTitle}</div>
+                                        </Card.Body>
+
+                                        <Card.Footer gap="3">
+                                            <Link href={slideInfo.slideUrl} target="_blank"
+                                                  rel="noopener noreferrer">
+                                                <IconButton className={css({
+                                                    backgroundColor: "white",
+                                                    outline: "1px solid",
+                                                    outlineColor: "black"
+                                                })} aria-label="Show slidev">
+                                                    <Presentation color="black"/>
+                                                </IconButton>
+                                            </Link>
+                                            <Link href={slideInfo.pdfUrl} target="_blank"
+                                                  rel="noopener noreferrer">
+                                                <IconButton className={css({
+                                                    backgroundColor: "white",
+                                                    outline: "1px solid",
+                                                    outlineColor: "black"
+                                                })} aria-label="Download pdf">
+                                                    <FileText color="black"/>
+                                                </IconButton>
+                                            </Link>
+                                        </Card.Footer>
+                                    </Card.Root>
+                                </li>
+                            ))}
+                    </ul>
+                </>
+            ))
+            }
         </>
     )
+}
+
+type SlideInfo = {
+    slideTitle: string;
+    slideUrl: string;
+    pdfUrl: string;
+    imageUrl: string;
+}
+
+type SlideGroup = {
+    Group: string;
+    list: slide[];
+}
+
+type slide = {
+    slide: string;
+    title: string;
+}
+
+function getSlideInfo(slide: string, title: string): SlideInfo {
+    return {
+        slideTitle: title,
+        slideUrl: `https://slide.moripa.nikomaru.dev/${slide}/index.html`,
+        pdfUrl: `https://slide.moripa.nikomaru.dev/${slide}/slidev-exported.pdf`,
+        imageUrl: `https://pub-f3e3168c75404090b6bd8358789607d8.r2.dev/${slide}/picture/1.png`,
+    }
 }
 
 declare global {
