0a9fd1707acbcb866fc32687d3621147c94b82d7
Nlkomaru <nikomaru@nikomaru.dev>
2024-06-29 14:23:23 +0900

    Fix slide name in package.json

--- resource/gen.sh
+++ resource/gen.sh
@@ -2,7 +2,7 @@ echo "スライド名を入力してください"
 read slide_name
 SLIDE_NAME=$slide_name
 echo "スライド名は$SLIDE_NAMEです"
-FILE_NAME="slide-$slide_name"
+FILE_NAME="docs-$slide_name"
 echo "スライドを生成します"
 rm -rf packages/$FILE_NAME
 cp -r packages/template packages/$FILE_NAME
